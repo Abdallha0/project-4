3b4a480c4d69ba11d4b0c08845026961058c5be2
Abdallha0 <164950184+Abdallha0@users.noreply.github.com>
2026-02-09 03:34:43 +0200

    ..

--- js/sign.js
+++ js/sign.js
@@ -10,8 +10,8 @@ const lnameLabel = document.querySelector(".lname-label")
 const emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const passwordRegex = /^(?=.*\d)(?=.*[a-zA-Z])[a-zA-Z0-9!#$%&?]{8,20}$/;
 const nameRegex = /^[a-zA-Z]{3,}$/;
-// const domain = "https://abdallha0.github.io/project-4"
-const domain = "http://127.0.0.1:3000/"
+const domain = "https://abdallha0.github.io/project-4"
+//const domain = "http://127.0.0.1:3000/"
 
 passUnvisableBtn.addEventListener("click", () => {
     passInput.type = "text"
