4cf9e5facb21c56e2f297949cfe837bf2daedef6
Abdallha0 <164950184+Abdallha0@users.noreply.github.com>
2026-02-09 04:02:01 +0200

    ..

--- js/home.js
+++ js/home.js
@@ -41,7 +41,7 @@ const productsSec = document.querySelector(".products-sec");
     const res = await fetch(domain + '/json/data.json');
     const data = await res.json();
 
-    document.writeln("res")
+    document.writeln("res", data)
 
     productsSec.innerHTML = data.map(i => {
      return createProductCard(i)
